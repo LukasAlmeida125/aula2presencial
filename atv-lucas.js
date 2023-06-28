139ee72648e121bf988e7d67bb3e6ec8cc117375
LukasAlmeida125 <101156177+LukasAlmeida125@users.noreply.github.com>
2023-06-28 14:54:29 -0300

    Add files via upload

---
+++ atv-lucas.js
@@ -0,0 +1,35 @@
+1-
+var numero1 = new Number(300);
+console.log(numero1);
+
+var numero2 = new Number(999);
+console.log(numero2)
+
+var numero3 = new Number(21);
+console.log(numero3);
+
+
+
+2-
+var test = 1000
+console.log(Number.EPSILON)
+
+var test2 = 150
+console.log( Number.MAX_SAFE_INTEGER)
+
+var test3= 400
+console.log(Number.MAX_VALUE)
+
+var test4= 200
+console.log(Number.MIN_VALUE)
+
+var numero = new Number(150)
+console.log(numero)
+numero = numero.toString()
+console.log(numero)
+
+3-
+var numero = new Number(150)
+console.log(numero)
+numero = numero.toString()
+console.log(numero)
